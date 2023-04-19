79618f74983b3b5796ef404090b69af42ea10678
Nathan Nieman <nnieman@anderson.edu>
2023-04-18 21:41:06 -0400

    Implement PING command

--- app/server.go
+++ app/server.go
@@ -15,9 +15,12 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+	connection, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+
+	_, err = connection.Write([]byte("+PONG\r\n"))
+	connection.Close()
 }
